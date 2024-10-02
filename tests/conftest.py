7aa89530e119d950d0294d4579a15b701173f627
ragucwa <ra.gucwa@gmail.com>
2024-10-02 11:31:28 +0200

    Cleanup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,4 @@
-from datetime import datetime
 import time
-import allure
 import pytest
 import requests
 from selenium import webdriver
@@ -14,26 +12,44 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture
-def driver(request, wait_for_grid):
+def driver(request):
     browser = request.config.getoption("--browser")
-    # if browser == "chrome":
-    #     driver_option = webdriver.ChromeOptions()
-    #     driver_option.add_argument("--window-size=1920,1080")
-    #     driver_option.add_argument("incognito")
-    #     driver_option.add_argument("--headless")
-    #     driver_option.add_argument("--disable-extensions")
-    #     driver_option.add_argument("--no-sandbox")
-    #     driver_option.add_argument("--disable-dev-shm-usage")
-    #     driver_option.experimental_options["prefs"] = {
-    #         "default_content_settings": {"images": 2}
-    #     }
-    #     driver = webdriver.Chrome(options=driver_option)
-    # elif browser == "firefox":
-    #     driver_option = webdriver.FirefoxOptions()
-    #     driver_option.add_argument("--width=1920,height=1080")
-    #     driver_option.add_argument("-private")
-    #     driver_option.add_argument("-headless")
-    #     driver = webdriver.Firefox(options=driver_option)
+    if "local" in browser:
+        driver = local_driver(browser)
+    else:
+        driver = remote_driver(browser)
+
+    yield driver
+
+    driver.quit()
+
+
+def local_driver(browser):
+    if browser == "chrome_local":
+        driver_option = webdriver.ChromeOptions()
+        driver_option.add_argument("--window-size=1920,1080")
+        driver_option.add_argument("incognito")
+        driver_option.add_argument("--headless")
+        driver_option.add_argument("--disable-extensions")
+        driver_option.add_argument("--no-sandbox")
+        driver_option.add_argument("--disable-dev-shm-usage")
+        driver_option.experimental_options["prefs"] = {
+            "default_content_settings": {"images": 2}
+        }
+        driver = webdriver.Chrome(options=driver_option)
+    elif browser == "firefox_local":
+        driver_option = webdriver.FirefoxOptions()
+        driver_option.add_argument("--width=1920,height=1080")
+        driver_option.add_argument("-private")
+        driver_option.add_argument("-headless")
+        driver = webdriver.Firefox(options=driver_option)
+    else:
+        raise Exception(f"Unsupported browser: {browser}")
+
+    return driver
+
+
+def remote_driver(browser, wait_for_grid):
     if browser == "chrome":
         driver_option = webdriver.ChromeOptions()
         driver_option.add_argument("--window-size=1920,1080")
@@ -54,8 +70,8 @@ def driver(request, wait_for_grid):
         )
     else:
         raise Exception(f"Unsupported browser: {browser}")
-    yield driver
-    driver.quit()
+
+    return driver
 
 
 @pytest.fixture(autouse=True)
@@ -63,23 +79,6 @@ def login_before_tests(driver, request):
     login_page = LoginPage(driver)
     login_page.open_login_page()
     login_page.log_in("standard_user", PASSWORD)
-
-
-# @pytest.hookimpl(tryfirst=True, hookwrapper=True)
-# def pytest_runtest_makereport(item, call):
-#     outcome = yield
-#     report = outcome.get_result()
-#     if report.failed:
-#         driver = item.funcargs["driver"]
-#         timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-#         screenshot_name = f"{item.name}_{timestamp}_screenshot.png"
-#         driver.save_screenshot(f"test_results/{screenshot_name}")
-#         with open(f"test_results/{screenshot_name}", "rb") as f:
-#             allure.attach(
-#                 f.read(),
-#                 name=screenshot_name,
-#                 attachment_type=allure.attachment_type.PNG,
-#             )
 
 
 @pytest.fixture
